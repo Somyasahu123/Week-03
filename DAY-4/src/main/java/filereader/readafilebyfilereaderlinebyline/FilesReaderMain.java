bf6c753aeea8f8e7265ab39d3cb90154986bb79b
Somyasahu123 <somyasahu221@gmail.com>
2025-02-09 23:07:58 +0530

    SOMYA SAHU ADD . builder-buffer-searching-challengeproblems

---
+++ DAY-4/src/main/java/filereader/readafilebyfilereaderlinebyline/FilesReaderMain.java
@@ -0,0 +1,8 @@
+package filereader.readafilebyfilereaderlinebyline;
+import java.io.*;
+
+public class FilesReaderMain {
+    public static void main(String[] args) throws IOException {
+        FilesReader.readFile();
+    }
+}
